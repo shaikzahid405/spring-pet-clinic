58ccaf3124572de111bb8e43415e30975c9d9fe1
shaikzahid405 <shaikzahid405@gmail.com>
2020-04-06 23:37:15 +0530

    Spring Pojo Model

---
+++ src/main/java/com/example/springdev/SpringpetClinicApplication.java
@@ -0,0 +1,14 @@
+package com.example.springdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringpetClinicApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringpetClinicApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/springdev/model/Person.java
@@ -0,0 +1,7 @@
+package com.example.springdev.model;
+
+public class Person {
+    private  String firstName;
+    private  String lastName;
+
+}
